ec0f5dcad684e88d67b403750f11759758f687dc
lightingft <evaldasl61@gmail.com>
2018-11-19 16:45:42 +0100

    Pawn

--- public/javascripts/pieces.js
+++ public/javascripts/pieces.js
@@ -23,15 +23,32 @@ function Pawn()
 	Piece.call(this);
 	
 	// Keep track if it's the first move for the Pawn
-	this.firstMove = false;
+	this.firstMove = true;
 	
 	this.getValidMoves = function() {
+		// We igonre the base case here because if a pawn reaches
+		// the end of the board, then it is no longer a pawn.
 		var moves = [];
-		
-		if (firstMove)
+
+		// Offset: if piece is white, then the offset is 1.
+		var offset = 1;
+
+		// If the piece is black, then the offset is -1
+		if (!this.isWhite)
+			offset = -1;
+	
+		// Valid move would be 1 forward
+		moves.push(coordinatesToCell(this.x, this.y+offset));
+
+		// If it's the first move for the pawn, then moving 2 forward
+		// is also a valid move
+		if (this.firstMove)
 		{
-			moves.push();
+			moves.push(coordinatesToCell(this.x, this.y+2*offset));
 		}
+
+		// Return moves array
+		return moves;
 	}
 }
 
