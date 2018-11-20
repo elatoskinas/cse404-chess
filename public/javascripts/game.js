d45b4d709f8ffdec6e179dc31ff89c005d8e3ecd
Calintheguy <gcalin224@gmail.com>
2018-11-20 22:24:07 +0100

    game states

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -9,4 +9,19 @@ function Game(id, p1, p2)
 	this.getID = function() { return this.id; }
 	
 	// Game State here
+	/* Game states:
+		0 = ongoing
+		1 = player 1 won
+		2 = player 2 won
+		3 = draw
+		4 = one of the players disconnected
+	*/
+	var gameState = 0;
+
+	// Booleans to indicate the check status for each player
+	var playerOneCheck = false;
+	var playerTwoCheck = false;
+
+	// A boolean to indicate which player is active, true = player one (white).
+	var activePlayer = true;
 }
