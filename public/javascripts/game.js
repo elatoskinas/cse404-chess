03280aee9616fe3067cced512662be645282370d
lightingft <evaldasl61@gmail.com>
2018-11-21 00:30:52 +0100

    getValidMoves validity checks for Pawn, King, Bishop

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -58,30 +58,30 @@ function Game(id, p1, p2)
 	this.populateBoard = function()
 	{
 		// White
-		this.board[0][0] = new Rook(0, 0, true); // A1
-		this.board[1][0] = new Knight(1, 0, true); // B1
-		this.board[2][0] = new Bishop(2, 0, true); // C1
-		this.board[3][0] = new Queen(3, 0, true); // D1
-		this.board[4][0] = new King(4, 0, true); // E1
-		this.board[5][0] = new Bishop(5, 0, true); // F1
-		this.board[6][0] = new Knight(6, 0, true); // G1
-		this.board[7][0] = new Rook(7, 0, true); // H1
+		this.board[0][0] = new Rook(true); // A1
+		this.board[1][0] = new Knight(true); // B1
+		this.board[2][0] = new Bishop(true); // C1
+		this.board[3][0] = new Queen(true); // D1
+		this.board[4][0] = new King(true); // E1
+		this.board[5][0] = new Bishop(true); // F1
+		this.board[6][0] = new Knight(true); // G1
+		this.board[7][0] = new Rook(true); // H1
 
 		// Black
-		this.board[0][7] = new Rook(0, 7, false); // A8
-		this.board[1][7] = new Knight(1, 7, false); // B8
-		this.board[2][7] = new Bishop(2, 7, false); // C8
-		this.board[3][7] = new Queen(3, 7, false); // D8
-		this.board[4][7] = new King(4, 7, false); // E8
-		this.board[5][7] = new Bishop(5, 7, false); // F8
-		this.board[6][7] = new Knight(6, 7, false); // G8
-		this.board[7][7] = new Rook(7, 7, false); // H8
+		this.board[0][7] = new Rook(false); // A8
+		this.board[1][7] = new Knight(false); // B8
+		this.board[2][7] = new Bishop(false); // C8
+		this.board[3][7] = new Queen(false); // D8
+		this.board[4][7] = new King(false); // E8
+		this.board[5][7] = new Bishop(false); // F8
+		this.board[6][7] = new Knight(false); // G8
+		this.board[7][7] = new Rook(false); // H8
 
 		// White & Black Pawns
 		for (var i = 0; i < 8; ++i)
 		{
-			this.board[i][1] = new Pawn(i, 1, true); // add White Pawn
-			this.board[i][6] = new Pawn(i, 6, false); // add Black Pawn
+			this.board[i][1] = new Pawn(true); // add White Pawn
+			this.board[i][6] = new Pawn(false); // add Black Pawn
 		}
 	}
 
