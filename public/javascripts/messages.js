54c8e8624bb87d7a24257cc4f0fe81f8345a0bc9
lightingft <evaldasl61@gmail.com>
2018-12-04 19:05:48 +0100

    Message Templates

--- public/javascripts/messages.js
+++ public/javascripts/messages.js
@@ -1,4 +1,10 @@
 (function(exports)
 {
-    
+    // Player A or Player B to Server: Click Tile
+    // Server to Player A or B: Select Piece Result
+    // Server to Client: Aborted Game
+    // Server to Client: White's Turn
+    // Server to Client: Black's Turn
+    // Server to Player A or B: Check
+    // Server to Player A & B: GameOver With Win/Stalemate/Loss result
 });
