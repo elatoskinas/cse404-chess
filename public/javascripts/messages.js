3f4c15a4cda132abd9425224d7b7b862bc1dad33
Calintheguy <gcalin224@gmail.com>
2018-12-04 19:19:30 +0100

    .

--- public/javascripts/messages.js
+++ public/javascripts/messages.js
@@ -3,8 +3,21 @@
     // Player A or Player B to Server: Click Tile
     // Server to Player A or B: Select Piece Result
     // Server to Client: Aborted Game
-    // Server to Client: White's Turn
+
+
     // Server to Client: Black's Turn
+    exports.O_PLAYER_TURN = {
+        type: "BLACK-TURN",
+        data: null
+    }
     // Server to Player A or B: Check
+    exports.O_CHECK = {
+        type: "CHECK",
+        data: null
+    }
     // Server to Player A & B: GameOver With Win/Stalemate/Loss result
+    exports.O_CHECKMATE = {
+        type: "CHECKMATE",
+        data: null
+    }
 });
