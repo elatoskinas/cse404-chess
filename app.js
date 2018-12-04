a104911a3c3a5536ac2003fcfcd61736baaaef4d
lightingft <evaldasl61@gmail.com>
2018-12-04 20:37:16 +0100

    WebSocket first steps

--- app.js
+++ app.js
@@ -14,21 +14,30 @@ app.use(express.static(__dirname + "/public"));
 app.use('/', indexRouter);
 app.use('/play', indexRouter);
 
-// --- WebSockets ---
-var websockets = []; // array keeping track of websockets
-var connectionID = 0; // keep track of next unique WebSocket Connection ID
 
-// TBD: Move these stats elsewhere
+// TBD: Move this stat in GameStatus JS file
 var gamesInitialized = 0;
 // -------------------------------
 
+// --- WebSockets ---
+var websockets = []; // array keeping track of websockets
+var connectionID = 0; // keep track of next unique WebSocket Connection ID
 var currentGame = new Game(gamesInitialized++); // keep track of current game
 
+// Server Creation
 var server = http.createServer(app).listen(port); // create server on port
 const wss = new websocket.Server( {server} ); // create WebSocket server
 
+// Listen for incoming WebSocket connections
 wss.on("connection", function connection(ws) {
     let connection = ws; // reference connection to ws
     connection.id = connectionID++; // assign unique ID, increment it for use for next connections
     let playerType = currentGame.addPlayer(connection); // true for White, false for Black
+    websockets[connection.id] = currentGame; // assign game to connection ID in WebSockets array
+
+    // debug
+    console.log("Player %s placed in game %s as %s", con.id, currentGame.id, playerType);
+
+    // inform the client about the pieces that will be controlled by the client
+    con.send((playerType) ? messages.S_PLAYER_WHITE : messages.S_PLAYER_BLACK);
 });
