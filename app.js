f86e8b381db97e4248b1e756bb92f3e93ac82e32
lightingft <evaldasl61@gmail.com>
2018-12-04 19:39:00 +0100

    ws begin

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var http = require("http");
+var ws = require("ws");
 
+// route path
 var indexRouter = require("./routes/index.js");
 
 var port = process.argv[2];
@@ -8,7 +10,19 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+// routes
 app.use('/', indexRouter);
 app.use('/play', indexRouter);
 
-http.createServer(app).listen(port);
+// --- WebSockets ---
+var websockets = []; // array keeping track of websockets
+var connectionID = 0; // each websocket has a unique ID
+
+var server = http.createServer(app).listen(port);
+const wss = new ws.Server( {server} );
+
+wss.on("connection", function connection(ws) {
+    ws.on("message", function incoming(message) {
+        console.log("X");
+    });
+});
