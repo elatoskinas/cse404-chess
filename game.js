ef27b63d53b974cd975d61eee9b70809f4c29e83
lightingft <evaldasl61@gmail.com>
2018-12-05 18:52:24 +0100

    game.js typos

--- game.js
+++ game.js
@@ -3,22 +3,23 @@ function Game(id)
 	this.id = id;
 	this.p1 = null;
 	this.p2 = null;
-	
+
 	this.getID = function() { return this.id; }
 
 	this.addPlayer = function(p)
 	{
-		if (p1 == null)
+		if (this.p1 == null)
 		{
-			p1 = p;
+			this.p1 = p;
 			return true; // white
 		}
 		
-		if (p2 == null)
+		if (this.p2 == null)
 		{
-			p2 = p;
+			this.p2 = p;
 			return false; // black
 		}
     }
 }
-	
+
+module.exports = Game;
