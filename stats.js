e8d2399cb4f7cf27158db92a02d5c249b998d395
lightingft <evaldasl61@gmail.com>
2018-12-10 14:23:31 +0100

    Stat tracking begin

--- stats.js
+++ stats.js
@@ -3,6 +3,7 @@
 var gameStats =
 {
     //since : Date.now(), /** Keep track when the object was created */
+    gamesInitialized: 0,
     ongoingGames: 0,
     gamesCompleted: 0,
     activeGamers: 0
