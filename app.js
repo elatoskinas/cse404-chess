995256b110882f291565de55b54528b1f300d6a2
Calintheguy <gcalin224@gmail.com>
2018-12-14 14:22:40 +0100

    REfactoring

--- app.js
+++ app.js
@@ -119,12 +119,12 @@ wss.on("connection", function connection(ws) {
                     connection.send(JSON.stringify(messages.O_SELECT_PIECE));
                     
                     // Update board for both players
-                    var checkmateStatus = socketGame.gameState.newTurn();
+                    var checkInfo = socketGame.gameState.newTurn();
+                    var checkStatus = checkInfo[1];
+                    var checkmateStatus = checkInfo[0];
                     // If a piece is moved successfully, switch the turn     
                     socketGame.p1.send(JSON.stringify(clickResponse));
                     socketGame.p2.send(JSON.stringify(clickResponse));
-
-                    var playerCheckStatus = socketGame.gameState.sendCheckStatus(); 
                     
                     if(playerCheckStatus!=null){
                         if(playerCheckStatus.data==true)
